901eb582bb9859d524e45c5fbfa3a844efccb460
matharris <matharris@sirrahtam.com>
2024-12-06 12:44:48 -0500

    ^ clean up header on mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="bg-slate-800">
-    <div class="container lg:max-w-4xl mx-auto py-4 flex flex-1 justify-between">
-      <a href="/" class="p-3 font-bold">{{ info.siteName }}</a>
+    <div class="container lg:max-w-4xl mx-auto py-4 md:flex flex-1 justify-between">
+      <a href="/" class="block p-3 font-bold">{{ info.siteName }}</a>
       <main-nav :links="links" class="hidden md:block"></main-nav>
       <main-nav :links="links" id="main-nav-mobile" class="md:hidden"></main-nav>
-      <button class="md:hidden" data-target="#main-nav-mobile" aria-controls="navbarToggleExternalContent" aria-expanded="false">
+      <!-- <button class="md:hidden" data-target="#main-nav-mobile" aria-controls="navbarToggleExternalContent" aria-expanded="false">
         Menu
-      </button>
+      </button> -->
     </div>
   </header>
 </template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <nav>
     <ul class="flex flex-auto flex-col md:flex-row ml-auto">
-      <li class="px-3" v-for="link in links" :key="link.name">
+      <li class="md:px-3" v-for="link in links" :key="link.name">
         <a :href="link.link" class="inline-block p-3 font-bold">{{ link.name }}</a>
       </li>
     </ul>
