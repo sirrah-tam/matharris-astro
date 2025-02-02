fee0a054774ec530ba96782aee35215665a4891c
matharris <matharris@sirrahtam.com>
2025-02-02 13:24:19 -0500

    ^ refactor & performance changes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,22 +1,20 @@
 <template lang="html">
-  <section id="projects" class="projects container lg:max-w-4xl mx-auto py-14 px-4 lg:px-0">
-    <div class="header flex mb-14">
-      <CodeBracketIcon class="size-10 inline-block text-white align-text-top mr-4" />
-      <h2 class="text-4xl">Dev Projects</h2>
+  <div class="header flex mb-14">
+    <CodeBracketIcon class="size-10 inline-block text-white align-text-top mr-4" />
+    <h2 class="text-4xl">Dev Projects</h2>
+  </div>
+  <div class="grid lg:grid-cols-3 gap-5 leading-normal">
+    <div v-for="project in projects">
+      <figure class="project mb-4">
+        <a :href="project.link" target="_blank">
+          <img :src="project.image" :alt="project.name">
+        </a>
+        <figcaption class="mt-4 font-bold">{{ project.name }}</figcaption>
+      </figure>
+      <p>Design by: <span v-html="project.designby"></span></p>
+      <p>Made with: {{ project.madewith }}</p>
     </div>
-    <div class="grid lg:grid-cols-3 gap-5 leading-normal">
-      <div v-for="project in projects" >
-        <figure class="project mb-4">
-          <a :href="project.link" target="_blank">
-            <img :src="project.image" :alt="project.name">
-          </a>
-          <figcaption class="mt-4 font-bold">{{ project.name }}</figcaption>
-        </figure>
-        <p>Design by: <span v-html="project.designby"></span></p>
-        <p>Made with: {{ project.madewith }}</p>
-      </div>
-    </div>
-  </section>
+  </div>
 </template>
 
 <script>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,30 +1,28 @@
 <template lang="html">
-  <section class="about container lg:max-w-4xl mx-auto py-14 px-4 lg:px-0">
-    <h1 class="text-7xl text-center">Resume</h1>
-    <div v-for="(value, key) in resume" :key="key">
-      <h2 class="text-5xl">{{ key[0].toUpperCase() + key.substring(1) }}</h2>
-      <div v-if="key == 'experience'">
-        <div v-for="experience in value" :key="experience.company">
-          <h3 class="text-3xl">{{ experience.company }}</h3>
-          <p v-if="experience.title">{{ experience.title }}</p>
-          <ul class="list-disc list-inside">
-            <li v-for="item in experience.description" :key="item">{{ item }}</li>
-          </ul>
-        </div>
-      </div>
-      <div v-else-if="key == 'education'">
-        <div v-for="education in value" :key="education.college">
-          <h3 class="text-3xl">{{ education.college }}</h3>
-          <p v-if="education.years">{{ education.years }}</p>
-          <p v-if="education.degree">{{ education.degree }}</p>
-          <p v-if="education.thesis">{{ education.thesis }}</p>
-        </div>
-      </div>
-      <div v-else-if="key == 'skills'" class="mt-9">
-        <p>{{ value[0] }}</p>
+  <h1 class="text-7xl text-center">Resume</h1>
+  <div v-for="(value, key) in resume" :key="key">
+    <h2 class="text-5xl mt-7 pb-3 border-b-2">{{ key[0].toUpperCase() + key.substring(1) }}</h2>
+    <div v-if="key == 'experience'">
+      <div v-for="experience in value" :key="experience.company">
+        <h3 class="text-3xl mt-6">{{ experience.company }}</h3>
+        <p v-if="experience.title">{{ experience.title }}</p>
+        <ul class="list-disc list-inside">
+          <li v-for="item in experience.description" :key="item">{{ item }}</li>
+        </ul>
       </div>
     </div>
-  </section>
+    <div v-else-if="key == 'education'">
+      <div v-for="education in value" :key="education.college">
+        <h3 class="text-3xl mt-6">{{ education.college }}</h3>
+        <p v-if="education.years">{{ education.years }}</p>
+        <p v-if="education.degree">{{ education.degree }}</p>
+        <p v-if="education.thesis">{{ education.thesis }}</p>
+      </div>
+    </div>
+    <div v-else-if="key == 'skills'" class="mt-9">
+      <p>{{ value[0] }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -37,15 +35,6 @@ export default {
 };
 </script>
 <style>
-h2 {
-  margin-top: 30px;
-  padding-bottom: 10px;
-  border-bottom: 2px solid white;
-}
-
-h3 {
-  margin-top: 30px;
-}
 
 p {
   font-size: 1rem;
