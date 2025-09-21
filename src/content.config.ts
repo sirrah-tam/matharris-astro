aec2787053171824b1e5d160b542ed8307204f2f
matharris <matharris@sirrahtam.com>
2025-09-21 14:03:09 -0700

    + books collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,17 @@
 import { defineCollection } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
 });
 
+const books = defineCollection({
+  loader: file("src/content/books/books.json" ),
+});
+
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog, books };
